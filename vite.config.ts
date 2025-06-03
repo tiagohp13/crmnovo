df468ed863939e555600493ae84e63470590a5cd
tiagohp13 <74267065+tiagohp13@users.noreply.github.com>
2025-06-04 00:09:06 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
+  root: '.', // garante que o Vite sabe onde está o index.html
+  build: {
+    outDir: 'dist'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })
