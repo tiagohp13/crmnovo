79cd6cb9bc7b743df7d44aaef683f532031c91f4
tiagohp13 <74267065+tiagohp13@users.noreply.github.com>
2025-06-04 09:57:28 +0100

    Create Layout.tsx

---
+++ src/Layout.tsx
@@ -0,0 +1,33 @@
+[09:52:51.377] Running build in Washington, D.C., USA (East) – iad1
+[09:52:51.377] Build machine configuration: 2 cores, 8 GB
+[09:52:51.394] Cloning github.com/tiagohp13/crmnovo (Branch: main, Commit: e858fc2)
+[09:52:51.402] Skipping build cache, deployment was triggered without cache.
+[09:52:51.661] Cloning completed: 266.000ms
+[09:52:51.997] Running "vercel build"
+[09:52:52.424] Vercel CLI 42.2.0
+[09:52:52.932] Installing dependencies...
+[09:53:02.120] 
+[09:53:02.121] added 64 packages in 9s
+[09:53:02.122] 
+[09:53:02.122] 7 packages are looking for funding
+[09:53:02.123]   run `npm fund` for details
+[09:53:02.161] Running "npm run build"
+[09:53:02.272] 
+[09:53:02.272] > crm-base@1.0.0 build
+[09:53:02.273] > vite build
+[09:53:02.273] 
+[09:53:02.594] [36mvite v4.5.14 [32mbuilding for production...[36m[39m
+[09:53:02.635] transforming...
+[09:53:03.718] [32m✓[39m 33 modules transformed.
+[09:53:03.895] rendering chunks...
+[09:53:03.901] computing gzip size...
+[09:53:03.909] [2mdist/[22m[32mindex.html                [39m[1m[2m  0.32 kB[22m[1m[22m[2m │ gzip:  0.24 kB[22m
+[09:53:03.910] [2mdist/[22m[2massets/[22m[36mindex-ab96a727.js  [39m[1m[2m159.10 kB[22m[1m[22m[2m │ gzip: 51.84 kB[22m
+[09:53:03.910] [32m✓ built in 1.31s[39m
+[09:53:03.983] Build Completed in /vercel/output [11s]
+[09:53:04.007] Deploying outputs...
+[09:53:05.476] 
+[09:53:05.635] Deployment completed
+[09:53:08.220] Uploading build cache [14.21 MB]...
+[09:53:08.548] Build cache uploaded: 330.664ms
+[09:53:10.768] Exiting build container
